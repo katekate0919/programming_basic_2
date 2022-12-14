7dc27789cb7782eb2e0758fcc593a233daa7209c
katekate0919 <91667759+katekate0919@users.noreply.github.com>
2022-12-14 23:50:22 +0900

    update change_word

--- change_word.js
+++ change_word.js
@@ -1,10 +1,12 @@
 function f(n){
+    // childNodesは配列のように扱うことができる
     let cs = n.childNodes;
     for(let i = 0; i < cs.length; i++){
       let c = cs[i];
       if(c.nodeType == Node.TEXT_NODE){
-        c.textContent = c.textContent.replace(/神戸/g, "KOBE ");
-        c.textContent = c.textContent.replace(/大学/g, "UNIVERSITY ");
+        c.textContent = c.textContent.replace(/です。/g, "です。覚えときや。");
+        c.textContent = c.textContent.replace(/しょう。/g, "しょう。読むだけじゃなくて手動かしや～。");
+        c.textContent = c.textContent.replace(/\(\d\)/g, "★");
       }
       else{
         f(c);
