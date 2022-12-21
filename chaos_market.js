0da40ce5c8a8be2097cafc20dfd736561e24619d
katekate0919 <91667759+katekate0919@users.noreply.github.com>
2022-12-21 20:23:13 +0900

    Update
    i want to change points, stars, and so on

--- chaos_market.js
+++ chaos_market.js
@@ -1,14 +1,14 @@
 function getRandom(min, max){
   let random = Math.floor(Math.random() * (max + 1 - min));
 
-  return (random * 100).toLocaleString();
+  return random * 100;
 }
 
 function randomPrice(n){
   let price = n.querySelectorAll('.important');
   for(let i = 0; i < price.length; i++){
-    price[i].innerText = "\xA5" + getRandom(1, 1000) + "　";
+    price[i].innerText = "\xA5" + getRandom(1, 1000).toLocaleString() + "　";
   }
 }
-  
+
 randomPrice(document.body);
